a03caca63f1a778e5ec56b70d4c802bd6c11682f
Nikolai <peckolai@gmail.com>
2021-06-19 15:53:06 +0300

    task 3 added

---
+++ task_3.rb
@@ -0,0 +1,12 @@
+require 'rspec/autorun'
+
+def reverse_words(str)
+  str.split.map { |word| word.size.even? ? word.reverse : word }.join(" ")
+end
+
+describe 'reverse_words' do
+  it 'should reverse every word in a given sentence' do
+    
+    expect(reverse_words('this is reverse string')).to eq('siht si reverse gnirts')
+  end
+end
